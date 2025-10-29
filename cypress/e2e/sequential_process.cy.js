82fb7f818051c27428e331fcb2453bed65733320
Tung Hoang <hoangthanhtung285@gmail.com>
2025-10-29 11:49:51 +1030

    poolcut and servicer upload implemented

--- cypress/e2e/sequential_process.cy.js
+++ cypress/e2e/sequential_process.cy.js
@@ -1,15 +1,64 @@
 describe('template spec', () => {
   it('passes', () => {
     cy.visit('https://dash-demo.sequenti.al/login')
-  })
-});
+    cy.get('[name="email"]').type('tung+demo@gosequential.com');
+    cy.get('[name="password"]').type('@Starlone321!');
+    cy.get('[name="remember"]').check();
+    cy.get('button.border').click();
+    cy.visit('https://dash-demo.sequenti.al/trusts/01k82kqy8njcm8p2v3prm24y0p')
+    cy.get('[data-testid="trust-nav-data"]').click();
+    cy.get('a.text-white').click();
+    cy.get('[name="data_reference_date"]').click();
+    cy.get('[name="data_reference_date"]').type('2025-09-30');
+    cy.get('[name="fileUpload.Poolcut"]').selectFile('/Users/tunghoang/Desktop/test data/metro/june_pcl.csv',{force: true});
+    cy.wait(5000);
+    cy.get('#mainForm button.submit').should('be.enabled');
+    cy.get('#mainForm button.submit').click();
+    cy.wait(30000);
+    cy.contains('Complete').should('be.visible');
+    cy.get('a.text-white').click();
+    cy.get('div.hidden.border-b button:nth-child(2)').click();
+    cy.get('a.text-white').click();
+    cy.get('[name="data_reference_date"]').click();
+    cy.get('[name="data_reference_date"]').type('2025-09-30');
+    cy.get('[name="fileUpload.PoolcutSecurity"]').selectFile('/Users/tunghoang/Desktop/test data/metro/june_pcs.csv',{force: true});
+    cy.wait(5000);
+    cy.get('#mainForm button.submit').should('be.enabled');
+    cy.get('#mainForm button.submit').click();
+    cy.wait(15000);
+    cy.contains('Complete').should('be.visible');
+    cy.get('a.text-white').click();
+    cy.get('div.hidden.border-b button:nth-child(3)').click();
+    cy.contains('Import Servicer Report').click();
+    cy.get('#mainForm [name="collections_period_end"]').select('2025-09-30');
+    cy.get('[name="fileUpload.Servicer"]').selectFile('/Users/tunghoang/Desktop/test data/metro/june_lmr.csv',{force: true});
+    cy.wait(5000);
+    cy.get('#mainForm button.submit').should('be.enabled');
+    cy.get('#mainForm button.submit').click();
+    cy.wait(5000);
+    cy.get('label.block').click();
+    cy.get('input.rounded').check();
+    cy.contains('Retry Import').click();
+    cy.wait(30000);
+    cy.contains('Complete').should('be.visible');
+    cy.contains('Return to list').click();
 
-it('sign in', function() {
-  cy.visit('https://dash-demo.sequenti.al/login')
-  cy.get('[name="email"]').type('tung+demo@gosequential.com');
-  cy.get('[name="password"]').type('@Starlone321!');
-  cy.get('[name="remember"]').check();
-  cy.get('button.border').click();
-  cy.get('button.items-center').click();
-  cy.get('form a.block').click();
-});
+    // cy.get('a.text-white').click();
+    // cy.get('div:nth-child(4) > div.flex > [data-testid="menu-item"]').click();
+    // cy.get('a[href="https://dash-demo.sequenti.al/trusts/01k82kqy8njcm8p2v3prm24y0p/reporting/bank-reconciliation"]').click();
+    // cy.get('div:nth-child(1) > div.min-w-0 > a.focus\\:outline-none > p.font-medium').click();
+    // cy.get('#mainForm button.float-left').click();
+    // cy.get('#mainForm label.relative span').click();
+    // cy.get('[name="fileUpload.t"]').click();
+    // cy.get('#mainForm button.submit').should('be.enabled');
+    // cy.get('#mainForm button.submit').click();
+    // cy.get('a[href="https://dash-demo.sequenti.al/trusts/01k82kqy8njcm8p2v3prm24y0p/reporting/bank-reconciliation"]').click();
+    // cy.get('div:nth-child(2) > div.min-w-0 > a.focus\\:outline-none > p.font-medium').click();
+    // cy.get('#mainForm button.float-left').click();
+    // cy.get('#mainForm label.relative span').click();
+    // cy.get('[name="fileUpload.t"]').click();
+    // cy.get('#mainForm button.submit').click();
+    // cy.get('button.items-center').click();
+    // cy.get('form[method="POST"] a.block').click();
+  })
+})
